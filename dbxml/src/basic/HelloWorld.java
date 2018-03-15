dce9c8595ea575c26f8365d8ff03710e77d847c4
bon-cheul Koo <boncheul92nd@naver.com>
2018-03-15 13:46:32 +0900

    HelloWorld예제

--- dbxml/src/basic/HelloWorld.java
+++ dbxml/src/basic/HelloWorld.java
@@ -38,5 +38,28 @@ public class HelloWorld {
     public static void main(String[] args) throws Throwable{
         // An empty string means an in-memory container, which
         // will not be persisted.
+        String containerName = "";
+        String content = "<hello>Hello World</hello>";
+        String docName = "doc";
+        XmlManager mgr = null;
+        XmlContainer cont = null;
+
+        try {
+            // All BDB XML programs require an XmlManager instance
+            mgr = new XmlManager();
+            cont = mgr.createContainer(containerName);
+            cont.putDocument(docName, content);
+
+            // Now, get the document
+            XmlDocument doc = cont.getDocument(docName);
+            String name = doc.getName();
+            String docContent = doc.getContentAsString();
+
+            // print it
+            System.out.println("Document name: " + name + "\nContent: " + docContent);
+
+        } catch (XmlException xe) {
+            System.err.println("XmlException during HelloWorld: " + xe.getMessage());
+        }
     }
 }
